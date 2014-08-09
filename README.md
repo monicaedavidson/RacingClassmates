56a574cdb2f8345cfb0262d950a783d1f48d2f1d
samhamm <samhamm@gmail.com>
2014-08-08 17:58:29 -0700

    needs JS debugging, but is close

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Sam and Monica's read me!
+
+
+THINGS TO FIX
+
+1. Why does Race! button re-init the values.
--- race.js
+++ race.js
@@ -1,36 +1,43 @@
-<script>
-/* The declaration of the RacerOne and RacerTwo variables in line 4 and creation of the Racer constructor in lines 6-11, are from a Code Fellows class with Brook Riggio on 8/6/2014. http://www.w3schools.com/jsref was also used as a reference for use of Math.pow and document.write. */
-
-function race {
-
-  var RacerOne, RacerTwo;
-  var RacerOneTotal = 0;
-  var RacerTwoTotal = 0;
-
-  function Racer(name, speed, focus) {
-    this.name = name;
-    this.speed = speed;
-    this.focus = focus;
+function Racer(name, speed, focus) {
+  this.name = name;
+  this.speed = speed;
+  this.focus = focus;
   }
 
-  RacerOne = new Racer (getElementByID('racerOne'), getElementByID('racerOneSpeed'), getElementbyID('currentRacerOneFocus'));
-  RacerTwo = new Racer (getElementByID('racerTwo'), getElementByID('racerTwoSpeed'), getElementbyID('currentRacerTwoFocus'));
+function Winner(name) {
+  this.name = racerOne;
+}
+
+function race() {
+
+  var racerOneFocus = 0;
+  var racerOneSpeed = 0;
+  var racerTwoFocus = 0;
+  var racerTwoSpeed = 0;
+  var racerOne, racerTwo;
+  var racerOneTotal = 0;
+  var racerTwoTotal = 0;
+  var oneFocus = parseInt(racerOne.focus);
+  var oneSpeed = parseInt(racerOne.speed);
+  var twoFocus = parseInt(racerTwo.focus);
+  var twoSpeed = parseInt(racerTwo.speed);
+  var raceResult1, raceResult2;
+
+  racerOne = new Racer (document.getElementsByName('racerOne')[0].value, document.getElementsByName('racerOneSpeed')[0].value, document.getElementsByName('racerOneFocus')[0].value);
+  racerTwo = new Racer (document.getElementsByName('racerTwo')[0].value, document.getElementsByName('racerTwoSpeed')[0].value, document.getElementsByName('racerTwoFocus')[0].value);
+
+  var raceResult1 = (racerOne.name + " and " + racerTwo.name + " have agreed to a 5-minute race...<br /><br />AND THEY'RE OFF!!!<br /><br />");
 
   //the i in the for loop is the number of minutes of the race
   //since RacerTwos can travel 20 meters/minute, I multiplied speed by 20m in the results
   for (var i = 1; i < 6; i++) {
 
-    var currentRacerOneFocus -= i; //private var
-    var currentRacerTwoFocus  -= i; // private var
+    var currentRacerOneFocus = currentRacerOneFocus - i;
+    var currentRacerTwoFocus = currentRacerTwoFocus - i;
 
-    RacerOneTotal += Math.pow(RacerOne.speed, currentRacerOneFocus);
-    RacerTwoTotal += Math.pow(RacerTwo.speed, currentRacerTwoFocus);
+    racerOneTotal += Math.pow(oneSpeed, oneFocus);
+    racerTwoTotal += Math.pow(twoSpeed, twoFocus);
 
-
-  //var raceResult = (racerOne.name + " and " + racerTwo.name + " have agreed to a 5-minute race...<br /><br />AND THEY'RE OFF!!!<br /><br />");
-
-  // After " + i + " minutes...<br />" RacerOne.name + " has traveled " + RacerOneTotal*200 + " meters!<br />" RacerTwo.name + " has traveled " + RacerTwoTotal*200 + " meters!<br /><br />" Winner.name + " won!);
-
-}
-
-</script>
+    console.log ("After " + i + " minutes..." + racerOne.name + " has traveled " + racerOneTotal + " meters!" + racerTwo.name + " has traveled " + racerTwoTotal + " meters!" + Winner.name + " won!");
+  }
+};
