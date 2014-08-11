af30e2d71dff5dddf44b11f4db8eb81bfed6b213
monicaedavidson <monicaedavidson@gmail.com>
2014-08-10 17:19:52 -0700

    Update race.js

--- race.js
+++ race.js
@@ -5,10 +5,10 @@
   var racerOne, racerTwo;
   var racerOneTotal = 0;
   var racerTwoTotal = 0;
-  var oneFocus = parseInt(racerOne.focus);
-  var oneSpeed = parseInt(racerOne.speed);
-  var twoFocus = parseInt(racerTwo.focus);
-  var twoSpeed = parseInt(racerTwo.speed);
+  var oneFocus = 0;
+  var oneSpeed = 0;
+  var twoFocus = 0;
+  var twoSpeed = 0;
   var raceResult1, raceResult2;
 
 function Racer(name, speed, focus) {
@@ -25,6 +25,11 @@ function race() {
 
   racerOne = new Racer (document.getElementsByName('racerOne')[0].value, document.getElementsByName('racerOneSpeed')[0].value, document.getElementsByName('racerOneFocus')[0].value);
   racerTwo = new Racer (document.getElementsByName('racerTwo')[0].value, document.getElementsByName('racerTwoSpeed')[0].value, document.getElementsByName('racerTwoFocus')[0].value);
+
+  oneFocus = parseInt(racerOne.focus);
+  oneSpeed = parseInt(racerOne.speed);
+  twoFocus = parseInt(racerTwo.focus);
+  twoSpeed = parseInt(racerTwo.speed);
 
   console.log (racerOne.name + " and " + racerTwo.name + " have agreed to a 5-minute race...<br /><br />AND THEY'RE OFF!!!<br /><br />");
 
